a2dcb65f4ec9e9a87f2085b6e1a4de9aec440ce8
Виталий Виталий <vitayaku91@gmail.com>
2020-06-12 11:20:33 +0300

    Начало проектв\а

---
+++ Linked List/DuplexItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List
+{
+    class DuplexItem<T>
+    {
+        public T Data { get; set; }
+        
+        public DuplexItem<T> Previous { get; set; }
+
+        public DuplexItem<T> Next { get; set; }
+    }
+}
